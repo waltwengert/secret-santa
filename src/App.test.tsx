ad3e82b9c1cd1bd53408b3f097bd106a68293c9b
Walt Wengert <walt.frederick.john@live.com>
2022-12-03 07:16:23 +1000

    fix breaking test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import App from './App';
 
 it('renders learn react link', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
+    const linkElement = screen.getByText(/secret santa/i);
     expect(linkElement).toBeInTheDocument();
 });
