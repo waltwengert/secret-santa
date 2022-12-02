022ac04e425e8a699b1dbbd2a16daa0243ee477d
Walt Wengert <walt.frederick.john@live.com>
2022-12-03 07:45:09 +1000

    basic user input setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import styled from 'styled-components';
 import { Header } from './header/Header';
+import { UserInput } from './input/UserInput';
 
 const StyledApp = styled.div`
     text-align: center;
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
 `;
 
 const App = () => {
     return (
         <StyledApp>
             <Header />
+            <UserInput />
         </StyledApp>
     );
 };
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -3,10 +3,6 @@ import { Label } from '../lib/ui/Label';
 
 const HeaderWrapper = styled.div`
     background-color: #282c34;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
     justify-content: center;
     font-size: calc(10px + 2vmin);
     color: white;
